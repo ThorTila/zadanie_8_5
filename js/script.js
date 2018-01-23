2ee722837f9c16b0edaa479a4e3c2a0b0595c8f3
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-01-23 22:38:57 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter value for a'),
+    h = prompt('Enter value for h'),
+    triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
